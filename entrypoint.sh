f0e7958c00d3eb0cd5e3d72120100d1caf51dff8
xuhao3e8 <xuhao3e8@gmail.com>
2025-01-01 23:37:30 +0800

    Slightly update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,6 @@ if [ "$1" == "launch" ]; then
     sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev eth0
     echo "Starting MAVROS on $FCU_URL, please set the FCU_URL in environment on docker, e.g. /dev/ttyUSB0:921600"
     nice --20 ros2 launch mavros px4.launch fcu_url:=$FCU_URL namespace:=/uav$DRONE_ID &
-    sleep 5
-    echo "Launching UWB node"
     echo "Starting swarmtal_control on $VO_TOPIC & $VO_IMU_TOPIC, please set the VO_TOPIC in environment, e.g. /d2vins/imu_propagation"
     nice --20 ros2 launch drone_commander px4.launch.py vo_topic:=$VO_TOPIC vo_imu_topic:=$VO_IMU_TOPIC  drone_id:=$DRONE_ID \
         config_path:=/drone_commander_config.yaml enable_odom_transformer:=false
